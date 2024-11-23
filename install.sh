4d14ee7785c76215479ab9e04350a7d50e24a108
Your Name <you@example.com>
2024-11-23 21:53:17 +0100

    BETA v1.0.1
    new flavour added

--- install.sh
+++ install.sh
@@ -1,13 +1,33 @@
-# downloading the zip
-
 # APT
-sudo apt install docker-compose unzip -y
+sudo apt-get update
+sudo apt-get install curl unzip ca-certificates zsh batcat lsd python3 python3-pip vim nano git -y
+
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # create the required folders
 mkdir ~/.desktopstudio
 mkdir ~/.desktopstudio/keys
 mkdir ~/.desktopstudio/packages
 
+# create VENV and pip requirements
+python3 -m venv ~/.desktopstudio/venv
+source ~/.desktopstudio/venv/bin/activate
+pip install rich
+
 # extract and copy files
-unzip release.zip
-cp -r release/defaults ~/.desktopstudio/
+cp -r funcs ~/.desktopstudio/funcs
+
+# add to the path the executable file
+cp DesktopStudio ~/.desktopstudio/DesktopStudio
+chmod +x ~/.desktopstudio/DesktopStudio
